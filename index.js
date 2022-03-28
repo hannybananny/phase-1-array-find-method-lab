18724e6013375c27330c694cb54adf5aaadd9382
Hannah <hannahmgrout@gmail.com>
2022-03-28 11:34:29 -0700

    commit tree 12944ee39c8085fbc37fcbc11f7e85eab79993d4

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(array){
+
+    let win = array.find(record => record.result === 'W')
+    console.log(win)
+    if (win){
+        return win.year
+    }
+}
+
